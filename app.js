2991a93cd4fbce12499a33f633f2fbd31ecc1e4c
Kareem <kpjackson27@hotmail.com>
2015-05-05 14:39:07 -0400

    third commit

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 'use strict';
 
 angular.module('calculator',[])
-	.controller('CalcCtrl', function($scope){
-		$scope.sub = 0;
-		$scope.tip = 0;
-		$scope.total = 0;
+	.controller('CalcCtrl', function($scope){	
 		$scope.calculate = function(){
-			$scope.sub = $scope.base * $scope.tax;
-			$scope.tip = ($scope.percentage/100) * $scope.sub;
-			$scope.total = $scope.sub + $scope.tip;
+			$scope.tax /= 100;
+			$scope.sub = parseFloat($scope.base) + parseFloat($scope.tax);
+			$scope.percentage /=100;
+			$scope.tip = parseFloat($scope.percentage) * parseFloat($scope.sub);
+			$scope.total = parseFloat($scope.sub) + parseFloat($scope.tip);
 		};
+
+		$scope.cancel = function(){
+			$scope.base = '';
+			$scope.tax = '';
+			$scope.percentage = '';
+
+		};
+
+		
 	});
