ae04e8ed9cd93595e10c9e32b55b341645a990f8
Kareem <kpjackson27@hotmail.com>
2015-05-05 15:13:58 -0400

    broken

--- app.js
+++ app.js
@@ -2,13 +2,22 @@
 
 angular.module('calculator',[])
 	.controller('CalcCtrl', function($scope){	
+		$scope.subtotal = [];
 		$scope.calculate = function(){
 			$scope.tax /= 100;
 			$scope.sub = parseFloat($scope.base) + parseFloat($scope.tax);
 			$scope.percentage /=100;
 			$scope.tip = parseFloat($scope.percentage) * parseFloat($scope.sub);
 			$scope.total = parseFloat($scope.sub) + parseFloat($scope.tip);
-		};
+			
+			
+			var num;
+			for (var i=0 ; i < $scope.subtotal.length; i++){
+				num = parseFloat($scope.subtotal[i]);
+					if(!isNan(num)) 
+						$scope.subtotal.push(parseFloat($scope.sub+=num));
+				}
+			};
 
 		$scope.cancel = function(){
 			$scope.base = '';
@@ -17,5 +26,5 @@ angular.module('calculator',[])
 
 		};
 
-		
+
 	});
